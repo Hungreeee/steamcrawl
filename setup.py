4540f4bcdaae63f4201f47a5c20b61f34a640412
Hung Nguyen <hung.6.nguyen@aalto.fi>
2023-05-06 21:37:29 +0300

    Edit setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
 from setuptools import setup
 
+with open("README.rst", "r") as f:
+    long_description = f.read()
+
 setup(
     name='steammy',
     version='0.1.0',
-    description='',
+    description='A package that helps extract Steam market data as pandas DataFrame for better readabilty and usage.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author='Hungreeee',
     author_email='hungmnguyen13102003@gmail.com',
     license='MIT',
-    install_requires=['requests', 'pandas', 'json'],
+    url='https://github.com/Hungreeee/steammy',
+    install_requires=['requests', 'pandas'],
     python_requires='>=3.5',
 )
